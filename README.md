3eb259299483ad835c5e74f5caf67fcbebe065ea
lsimonen1 <lsimonen@wisc.edu>
2023-09-20 22:40:06 -0500

    made into reactapp

--- README.md
+++
@@ -1,2 +0,0 @@
-# lsimonen1.github.io
-This is a personal website for Lucas Simonen, I am showing off cs and digital media related projects here.
---
+++ my-new-app/src/App.js
@@ -0,0 +1,21 @@
+import TabButton from "./components/TabButton";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="topHalf">
+      <TabButton title = 'Portfolio' />
+      <TabButton title = 'About Me' />
+      <TabButton title = 'Download Resume' />
+      </div>
+      <header className="App-header">
+        <p>
+          Lucas Simonen
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
